f4ae00153354b4e3eb272e41a689fcea69cbdde3
marcpanel <marc97oller@gmail.com>
2024-02-28 12:24:30 +0100

    Update Project.jsx

--- src/components/basics/Project.jsx
+++ src/components/basics/Project.jsx
@@ -9,7 +9,7 @@ export const Project = ({ title, image, body, advancedBody, github=null, stack, 
             animate={{ opacity: 1 }} initial={{ opacity: 0 }}
             className={"flex gap-4 flex-col w-full justify-center bg-white p-4 rounded-lg items-center cursor-pointer gap-2 shadow"}
         >
-            <div className="flex flex-row gap-2">
+            <div className="flex flex-col md:flex-row gap-2">
                 <div className="flex flex-col gap-2">
                     <div className="flex flex-row gap-2 items-center">
                         <IoArrowBackOutline className="text-2xl hover:cursor-pointer hover:scale-110 transition" onClick={onClick} />
@@ -35,7 +35,7 @@ export const Project = ({ title, image, body, advancedBody, github=null, stack, 
                     </div>
                 </div>
 
-                <img src={image} alt="logo" className={`flex h-[350px] items-center align-center rounded-lg object-cover`} />
+                <img src={image} alt="logo" className={`flex h-[250px] md:h-[350px] items-center align-center rounded-lg object-contain md:object-cover`} />
             </div>
 
         </motion.div>
